0bfa1787ede1a6cc950cac34ebbd5d573febf3b2
Charles-Antoine Bellerive <charles.antoine.bellerive.dev@gmail.com>
2021-01-28 15:37:59 -0500

    Random bot movement

--- MontrealGameJam2021/Assets/Scripts/GameLogic/PlayerSpawner.cs
+++ MontrealGameJam2021/Assets/Scripts/GameLogic/PlayerSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.Player;
 using Photon.Pun;
 using UnityEngine;
 
@@ -7,16 +8,44 @@ public class PlayerSpawner : MonoBehaviourPun
 {
     //Todo : Change to personnal camera
     [SerializeField] private Transform MainCamera;
-    
+    [SerializeField] private Bounds MapBounds;
     public void Start()
     {
+        SpawnPlayers();
+        SpawnBots();
+    }
+
+    private void SpawnPlayers()
+    {
         Object player;
-        
+
         if (PhotonNetwork.IsConnected)
             player = PhotonNetwork.Instantiate("Prefabs/Player", new Vector3(0, 0, 0), Quaternion.identity);
         else
             player = Instantiate(Resources.Load("Prefabs/Player"));
 
         ((GameObject) player).GetComponent<PlayerMovement>().Camera = MainCamera;
+    }
+
+    private void SpawnBots()
+    {
+        Object bot;
+        
+        if (PhotonNetwork.IsMasterClient)
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                bot = PhotonNetwork.Instantiate("Prefabs/Bot", new Vector3(0, 0, 0), Quaternion.identity);
+                ((GameObject) bot).GetComponent<BotMovement>()._bounds = MapBounds;
+            }
+        }
+        else if (!PhotonNetwork.IsConnected)
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                bot = Instantiate(Resources.Load("Prefabs/Bot"));
+                ((GameObject) bot).GetComponent<BotMovement>()._bounds = MapBounds;            
+            }
+        }
     }
 }
--- MontrealGameJam2021/Assets/Scripts/Player/BotMovement.cs
+++ MontrealGameJam2021/Assets/Scripts/Player/BotMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Linq;
 using Photon.Pun;
 using UnityEngine;
 using UnityEngine.AI;
@@ -10,20 +12,51 @@ namespace Assets.Scripts.Player
     {
         private NavMeshAgent _agent;
         private Animator _animator;
-        
-        public GameObject target;
 
+        [SerializeField]
+        private GameObject _debugtarget;
+
+        private bool hasDestination = false;
+
+        public Bounds _bounds;
+        
         public void Start()
         {
             _agent = GetComponent<NavMeshAgent>();
             _animator = GetComponent<Animator>();
         }
 
+        private IEnumerator Walk()
+        {
+            Debug.Log("Starting walking");
+            int time = Random.Range(2, 8);
+            Debug.Log($"Waiting for {time} seconds");
+            yield return new WaitForSeconds(time);
+            Debug.Log("Ended walking");
+            hasDestination = false;
+        }
+
         public void Update()
         {
-            _agent.SetDestination(target.transform.position);
-
+            if (!hasDestination)
+            {
+                _agent.SetDestination (new Vector3(
+                    Random.Range(_bounds.min.x, _bounds.max.x),
+                    Random.Range(_bounds.min.y, _bounds.max.y),
+                    Random.Range(_bounds.min.z, _bounds.max.z)
+                ));
+                
+                //Allows to see target position
+                if(_debugtarget != null)
+                    _debugtarget.transform.position = _agent.destination;
+                
+                Debug.Log("Setting random destination");
+                StartCoroutine(Walk());
+                hasDestination = true;
+            }
+            
             _animator.SetBool("isRunning", _agent.velocity.magnitude >= 1);
+            
         }
     }
 }
