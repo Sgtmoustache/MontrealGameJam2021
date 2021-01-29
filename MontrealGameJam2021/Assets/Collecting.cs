d1fa70247169072c29afa4af3c93bd4cbcca03bf
Volker37 <vinxy333@gmail.com>
2021-01-29 03:00:58 -0500

    test2

---
+++ MontrealGameJam2021/Assets/Collecting.cs
@@ -0,0 +1,26 @@
+using System.Linq.Expressions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collecting : Interactable {
+
+    [SerializeField] int objectID;
+
+    public bool canBePick = true;
+    
+    public override void Interact(GameObject player){
+        if(canBePick && !(Inventory.HasObject())){
+            Debug.Log("PLAYER GRAB");
+            Inventory.setObject(objectID, this.gameObject);
+            
+            this.gameObject.transform.SetParent(player.transform);
+            this.gameObject.transform.localPosition = new Vector3(0.0f, 8.0f, 0.0f);
+
+            canBePick = false;
+        }
+    }
+    public override void beInteractable(){
+        canBePick = true;
+    }
+}
