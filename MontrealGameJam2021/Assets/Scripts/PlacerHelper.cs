08a859c095836526f9e3f8703253d1a778bea362
Charles-Antoine Bellerive <charles.antoine.bellerive.dev@gmail.com>
2021-01-29 16:50:17 -0500

    Fixing some issue with visibility

---
+++ MontrealGameJam2021/Assets/Scripts/PlacerHelper.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public static class PlacerHelper
+    {
+        public static void Ground(Transform gameObject, float thickness)
+        {
+            RaycastHit hit;
+
+            Vector3 origin = gameObject.position;
+
+            int layerMask = 1 << 8;
+            layerMask = ~layerMask;
+        
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(origin, Vector3.down, out hit, Mathf.Infinity, layerMask))
+            {
+                
+            }
+            else
+            {
+            }
+        }
+    }
+}
