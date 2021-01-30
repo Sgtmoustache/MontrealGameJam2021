06327fae584b126e9401918eee2d095c8073ebb5
Charles-Antoine Bellerive <charles.antoine.bellerive.dev@gmail.com>
2021-01-29 21:01:03 -0500

    Missing }

--- MontrealGameJam2021/Assets/Scripts/Collecting.cs
+++ MontrealGameJam2021/Assets/Scripts/Collecting.cs
@@ -7,22 +7,26 @@ using Photon.Pun;
 public class Collecting : Interactable {
 
     [SerializeField] int objectID;
-    
-    public override void Interact(GameObject player){
+
+    public override void Interact(GameObject player)
+    {
         Inventory inventory = player.GetComponent<Inventory>();
-        if(inventory){
-            if(!(inventory.HasObject())){
+        if (inventory)
+        {
+            if (!(inventory.HasObject()))
+            {
                 Debug.Log("PLAYER GRAB");
                 photonView.RPC("SetLock", RpcTarget.All, true);
                 photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
                 inventory.setObject(objectID, this.gameObject);
 
-            this.gameObject.transform.SetParent(PlayerSpawner.LocalPlayer.transform);
-            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            this.gameObject.transform.localPosition = new Vector3(0.0f, 8.0f, 0.0f);
-            
+                this.gameObject.transform.SetParent(PlayerSpawner.LocalPlayer.transform);
+                gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                this.gameObject.transform.localPosition = new Vector3(0.0f, 8.0f, 0.0f);
+            }
         }
     }
+
     public override void beInteractable(){
         gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         photonView.RPC("SetLock", RpcTarget.All, false);
