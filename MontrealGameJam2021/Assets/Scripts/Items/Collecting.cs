3613633c919c2e9f8f827a102782a61b114769ad
Charles-Antoine Bellerive <charles.antoine.bellerive.dev@gmail.com>
2021-01-30 20:13:36 -0500

    Say rigidbody state

--- MontrealGameJam2021/Assets/Scripts/Items/Collecting.cs
+++ MontrealGameJam2021/Assets/Scripts/Items/Collecting.cs
@@ -16,29 +16,35 @@ public class Collecting : Interactable {
         {
             if (!(inventory.HasItem()))
             {
-                ;
                 base.Interact(player);
                 inventory.SetItem(gameObject.GetComponent<ItemInfo>().Collectibles, gameObject);
                 Vector3 vec = player.transform.position;
                 gameObject.transform.SetParent(PlayerSpawner.LocalPlayer.transform);
-                gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-
-                gameObject.transform.position = new Vector3(vec.x, (vec.y + 1.0f), vec.z);
+                
+                photonView.RPC("SetRigibodyConstraint", RpcTarget.All, RigidbodyConstraints.FreezeAll);
+                
+                gameObject.transform.position = new Vector3(vec.x, (vec.y + 2.2f), vec.z);
                 if(player.GetComponent<PlayerInfo>().PlayerType == "Student")
                     GameManager.TeacherScore -= 20 ; 
             }
         }
     }
 
+    [PunRPC]
+    public void SetRigibodyConstraint(RigidbodyConstraints constraints)
+    {
+        gameObject.GetComponent<Rigidbody>().constraints = constraints;
+    }
+
     public override void beInteractable(){
-        gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         photonView.RPC("SetLock", RpcTarget.All, false);
+        photonView.RPC("SetRigibodyConstraint", RpcTarget.All, RigidbodyConstraints.None);
         canBePick = true;
     }
 
     public void Disable() {
-        gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         photonView.RPC("SetLock", RpcTarget.All, false);
+        photonView.RPC("SetRigibodyConstraint", RpcTarget.All, RigidbodyConstraints.None);
         canBePick = false;
     }
 
