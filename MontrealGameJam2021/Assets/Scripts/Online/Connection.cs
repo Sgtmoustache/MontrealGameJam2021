1e1303ee44260a238321e79f53bb61743301768a
Charles-Antoine Bellerive <charles.antoine.bellerive.dev@gmail.com>
2021-01-30 22:04:59 -0500

    E... nfin on peut loader un serveur en solo

--- MontrealGameJam2021/Assets/Scripts/Online/Connection.cs
+++ MontrealGameJam2021/Assets/Scripts/Online/Connection.cs
@@ -43,7 +43,6 @@ namespace Menus
             if (PhotonNetwork.IsConnected)
             {
                 PhotonNetwork.JoinRandomRoom();
-                
             }
             else
             {
