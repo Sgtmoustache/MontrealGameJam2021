46cf895d8f426c85f2107f668614f9c7f2429d92
Volk <vinxy333@gmail.com>
2021-01-30 21:29:59 -0500

    TEsting 1 2 3 testintg

--- MontrealGameJam2021/Assets/Scripts/Items/Dropping.cs
+++ MontrealGameJam2021/Assets/Scripts/Items/Dropping.cs
@@ -24,43 +24,44 @@ public class Dropping : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!playerInfo.isLocal)
-        {
-            Inventory inventory = this.gameObject.GetComponent<Inventory>();
-            if(inventory.HasItem()){
-                if (Input.GetKeyDown(KeyCode.Q)){
-                    GameObject obj = inventory.GetItemGameObject();
-                    inventory.ClearItem();
-                    Vector3 vec = this.gameObject.transform.localPosition;
-                    Collecting collect = obj.GetComponent<Collecting>();
-                    collect.beInteractable();
-                    obj.transform.SetParent(null);
-                    obj.transform.localPosition = new Vector3(vec.x, 2.0f, vec.z);
-                    if(this.gameObject.GetComponent<PlayerInfo>().PlayerType == "Student")
-                        GameManager.TeacherScore += 20 ; 
-                }
-                if(this.gameObject.GetComponent<PlayerInfo>()?.PlayerType == "Student"){
-                    Vector3 targetPosition = new Vector3(10f, -1f , -26f);
-                    targetPosition.y = arrow.transform.position.y;
-                    arrow.gameObject.SetActive(true);
-                    arrow.LookAt(targetPosition);
-                }
-                else{
-                    Vector3 targetPosition = new Vector3(-19f, 0f , 0f);
-                    targetPosition.y = transform.position.y;
-                    arrow.gameObject.SetActive(true);
-                    arrow.LookAt(targetPosition);
-                }
+        Inventory inventory = this.gameObject.GetComponent<Inventory>();
+        if(inventory.HasItem()){
+            if (Input.GetKeyDown(KeyCode.Q)){
+                GameObject obj = inventory.GetItemGameObject();
+                inventory.ClearItem();
+                Vector3 vec = this.gameObject.transform.localPosition;
+                Collecting collect = obj.GetComponent<Collecting>();
+                collect.beInteractable();
+                obj.transform.SetParent(null);
+                obj.transform.localPosition = new Vector3(vec.x, 2.0f, vec.z);
+                if(this.gameObject.GetComponent<PlayerInfo>().PlayerType == "Student")
+                    GameManager.TeacherScore += 20 ; 
+            }
+            if(this.gameObject.GetComponent<PlayerInfo>()?.PlayerType == "Student"){
+                Vector3 targetPosition = new Vector3(10f, -1f , -26f);
+                targetPosition.y = arrow.transform.position.y;
+                arrow.gameObject.SetActive(true);
+                arrow.LookAt(targetPosition);
             }
             else{
-                arrow.gameObject.SetActive(false);
+                Vector3 targetPosition = new Vector3(-19f, 0f , 0f);
+                targetPosition.y = transform.position.y;
+                arrow.gameObject.SetActive(true);
+                arrow.LookAt(targetPosition);
             }
+        }
+        else{
+            arrow.gameObject.SetActive(false);
+        }
 
 
-            if (Input.GetKeyDown(KeyCode.T)){
-                Debug.Log(GameManager.StudentScore);
-                Debug.Log(GameManager.TeacherScore);
-            }
+        if (Input.GetKeyDown(KeyCode.T)){
+            Debug.Log(GameManager.StudentScore);
+            Debug.Log(GameManager.TeacherScore);
+        }
+
+        if(!playerInfo.isLocal)
+        {
 
             RaycastHit hit;
 
